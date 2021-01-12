708c0f006bbd6b547c564f968f485f32f1b1f68a
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2021-01-12 11:27:47 +0800

    SETUP VUE

---
+++ resources/js/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="parent__body">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+};
+</script>
+
+<style lang="scss">
+@import "../../sass/variables.scss";
+
+.parent__body {
+  width: 100%;
+  height: 100vh;
+  background-color: $white;
+}
+</style>
---
+++ resources/views/spa.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FDTP SYSTEMS</title>
+    <style>
+        *,
+        html {
+            box-sizing: border-box;
+            padding: 0;
+            margin: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="app">
+        <app></app>
+    </div>
+    <script src="http://localhost:8080/js/app.js"> </script>
+</body>
+
+</html>
